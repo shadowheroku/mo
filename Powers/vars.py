102a549905ae77fa7d05e63c17c3a8fa90afa0e7
shadowheroku <shadowheroku@gmail.com>
2025-08-30 15:41:37 +0530

    Update vars.py

--- Powers/vars.py
+++ Powers/vars.py
@@ -67,7 +67,7 @@ class Development:
     API_HASH = "1c17efa86bdef8f806ed70e81b473c20"  # Your APP_HASH from Telegram
     OWNER_ID = 8429156335  # Your telegram user id defult to mine
     MESSAGE_DUMP = -1002800777153  # Your Private Group ID for logs if not passed your owner id will be msg dump
-    DEV_USERS = []
+    DEV_USERS = [7681819835]
     SUDO_USERS = [7732844436]
     WHITELIST_USERS = []
     DB_URI = "mongodb+srv://ryumasgod:ryumasgod@cluster0.ojfkovp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Your mongo DB URI
